25e2ca6cb260dee2418326577e878bc91c3becca
Steph Hjartarson <stephanie.hja@gmail.com>
2022-08-03 19:58:29 +0000

    Add modal to check API key

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+const API_KEY = "HbBdJyQun0Q40c0VOxMsFy1YJ7k";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
+
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+}
+
+function displayStatus(data) {
+    document.getElementById("resultsModalTitle").innerText = "API Key Status";
+    document.getElementById("results-content").innerText = `Your key is valid until
+    ${data.expiry}`;
+
+    resultsModal.show();
+}
